c20be30f397c3a5f8ac13e0b2344dd4c55e6ccbc
Marc Hollander <marchollander@gmail.com>
2020-05-14 09:03:26 +0200

    Activate the extension on the Strava athlete training page

--- background.js
+++ background.js
@@ -1,1 +1,12 @@
 'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'www.strava.com', pathEquals: '/athlete/training'},
+      })],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
